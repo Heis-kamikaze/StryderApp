df2cc3eab3b5079654645df60bc4fe9414a59bd9
Heis-kamikaze <gerrardthegrayfoxx688@gmail,com>
2024-07-08 18:47:44 +0100

    - unneccessary import statement in message container comp

--- client/src/components/messageContainer/MessageContainer.jsx
+++ client/src/components/messageContainer/MessageContainer.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import useGetConversations from "../../hooks/useGetConversations";
 import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
